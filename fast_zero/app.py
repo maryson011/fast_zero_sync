1533d65573a928e9c0f83448496f2e5a2656c717
maryson011 <maryson011@gmail.com>
2024-10-31 07:22:40 -0300

    desacoplamento da conexão com banco de app

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,12 +1,11 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from fast_zero.models import User
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
-from fast_zero.settings import Settings
+from fast_zero.database import get_session
 
 app = FastAPI()
 
@@ -20,34 +19,33 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def crate_user(user: UserSchema):
-    engine = create_engine(Settings().DATABASE_URL)
+    session = get_session()
+    
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
 
-    with Session(engine) as session:
-        db_user = session.scalar(
-            select(User).where(
-                (User.username == user.username) | (User.email == user.email)
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
             )
-        )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='E-mail already exists',
+            )
 
-        if db_user:
-            if db_user.username == user.username:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail='Username already exists',
-                )
-            elif db_user.email == user.email:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail='E-mail already exists',
-                )
+    db_user = User(
+        username=user.username, email=user.email, password=user.password
+    )
 
-        db_user = User(
-            username=user.username, email=user.email, password=user.password
-        )
-
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
 
     return db_user
 
